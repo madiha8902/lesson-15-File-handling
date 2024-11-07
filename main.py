85afbd0f7c936f666ddd06a3a1c139c7558c129c
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-11-07 20:14:04 +1100

    File Handling

---
+++ main.py
@@ -0,0 +1,14 @@
+# file=open("abc.txt")
+
+# print(file.read())
+
+# file=open("abc.txt","w")
+# file.write("and my favortie language in pyton")
+
+# file.close("abc.txt","r")
+# print(file.read())
+file=open("abc.txt","a")
+file.write(" my name is madiha")
+flie=open("abc.txt","r")
+print(flie.read())
+file.close()
